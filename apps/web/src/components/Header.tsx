ecc16faeefd9af9e938cb291cfef0f1722820b2e
huslen0212 <huslen.battogtoh@yahoo.com>
2025-11-18 15:59:24 +0800

    icon

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@/components/ui/button';
 import { PlusIcon, User } from 'lucide-react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center p-6 bg-white text-black shadow-md">
-      <Link href="/">
+      <Link href="/" className="flex items-center gap-3">
+        <Image src="/sharnom.png" alt="Logo" width={60} height={60} />
+
         <h1 className="text-3xl font-bold tracking-tight font-sans">Шар ном</h1>
       </Link>
 
@@ -16,6 +19,7 @@ export default function Header() {
             <span>Нэмэх</span>
           </Button>
         </Link>
+
         <Button className="flex items-center gap-2 bg-black text-white hover:bg-white hover:text-black border border-black transition">
           <User className="w-5 h-5" />
           <span>Нэвтрэх</span>
